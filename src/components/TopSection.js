59e8b16b8ee74c86a7cf5eb93fc0f9f418d1de18
UTPAL KUMAR <utpal.kumar74@gmail.com>
2022-08-28 00:12:30 +0530

    Age format fix

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -3,9 +3,21 @@ import React from "react";
 
 const agguSmile = require('../imgs/aggu-smiles.jpg')
 
-function getYears() {
-    var differenceInTime = Math.abs(new Date() - new Date(2022, 6, 16));
-    return Math.ceil(differenceInTime / (1000 * 60 * 60 * 24))
+function getAgeInformation() {
+    const BIRTH_DATE = new Date(2022, 6, 16);
+    const TODAY = new Date()
+
+    const yearDiff = Math.abs(TODAY.getFullYear() - BIRTH_DATE.getFullYear())
+    const monthDiff = Math.abs(TODAY.getMonth() - BIRTH_DATE.getMonth())
+    const dayDiff = Math.abs(TODAY.getDate() - BIRTH_DATE.getDate())
+
+    if (yearDiff === 0) {
+        return `${monthDiff} month, ${dayDiff} days old`
+    } else if (monthDiff === 0) {
+        return `${dayDiff} days old`
+    } else {
+        return `${yearDiff} years, ${monthDiff} month old`
+    }
 }
 
 export default function TopSection() {
@@ -14,7 +26,7 @@ export default function TopSection() {
             <img className="profile-img" src={agguSmile} alt="Aggu smiling"></img>
             <div className="name_age">
                 <h3>@Aggu</h3>
-                <p className="age"><span> {getYears()} </span> days old</p>
+                <p className="age"><span>{getAgeInformation()}</span></p>
             </div>
         </div>
     )
